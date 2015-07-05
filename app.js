d285a8537bd07d1a42a1658ffb467eee2cbf063c
Evgeny Bordunov <feugenix@yandex-team.ru>
2015-07-06 02:47:17 +0300

    images

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
+
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    var num = Math.floor(Math.random() * 10);
+
+    fs.readFile(__dirname + '/img/test' + num + '.png', function(err, file) {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end(err);
+        }
+
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.end(buffile.toString('utf8'));
+    });
 }).listen(process.env.PORT || 5000);
